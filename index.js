2d26498df5529e7e22c7bc9ea2c18d4ad6b9f96b
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-18 16:42:11 -0700

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+const dodger = document.getElementById("dodger"); 
+document.addEventListener("keydown", function(event) {
+    console.log(event);
+  }); 
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+
+      dodger.style.left = `${left - 1}px`;
+    }
+  });  
+
+ 
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keyup", function(event) {
+    console.log(event);
+  }); 
+
+  document.addEventListener("keyup", function(event) {
+    if (event.key === "ArrowRight") {
+      const rightNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(rightNumbers, 10);
+
+      dodger.style.left = `${left + 1}px`;
+    }
+  }); 
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  } 
+ 
